788e6da50c57d9e2e1e25f02e26ee0abc8e48391
Danya1910 <shirniy.tonny@gmail.com>
2026-06-18 14:36:19 +0300

    split details & chart states

--- app/src/main/java/com/example/cryptotrack/presentation/states/DetailsState.kt
+++ app/src/main/java/com/example/cryptotrack/presentation/states/DetailsState.kt
@@ -3,8 +3,13 @@ package com.example.cryptotrack.presentation.states
 import com.example.cryptotrack.domain.model.CoinDetails
 import com.example.cryptotrack.domain.model.CoinsChartList
 
-data class DetailsScreenStates(
+data class DetailsState(
     val details: CoinDetails? = null,
+    val isLoading: Boolean = false,
+    val error: String? = null,
+)
+
+data class ChartState(
     val chart: CoinsChartList? = null,
     val isLoading: Boolean = false,
     val error: String? = null,
